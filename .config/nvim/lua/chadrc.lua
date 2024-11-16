83cc53142cb9534622e5b88063bf286782c9c8fc
Alexander Järnström <alexander.jarnstrom@gmail.com>
2024-11-16 19:23:54 +0100

    changed nvim theme to everforest

--- .config/nvim/lua/chadrc.lua
+++ .config/nvim/lua/chadrc.lua
@@ -6,8 +6,8 @@
 local M = {}
 
 M.base46 = {
-	theme = "yoru",
-  transparency = true,
+	theme = "everforest_light",
+  transparency = false,
 	-- hl_override = {
 	-- 	Comment = { italic = true },
 	-- 	["@comment"] = { italic = true },
