0b6c286d5a1eb178ad29fba62c895d5669c61f4b
Alexander Järnström <alexander.jarnstrom@gmail.com>
2025-03-06 13:10:30 +0100

    Better monitor handling, removed vpn option

--- .config/i3/scripts/monitor.sh
+++ .config/i3/scripts/monitor.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-echo $DOCKED_PRIMARY_MONITOR
-
-echo $(xrandr | grep -w connected | grep $DOCKED_PRIMARY_MONITOR)
+if xrandr | grep -q $1; then
+  xrandr --output $1 --auto --left-of $2 --primary
+  xrandr --output $2 --mode 1400x900 --scale 1
+else 
+  xrandr --output $1 --off
+  xrandr --output $2 --mode 1920x1200 --primary
+fi
